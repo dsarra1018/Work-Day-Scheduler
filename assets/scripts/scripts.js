53ea6f452553d108f45aaf19d9387431d03c1c5a
Darren Sarra <adriansarra18@gmail.com>
2020-03-01 22:41:20 -0500

    Added listeners for all buttons. setItems into the localStorage.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    init();
+
 
     // Displaying todays date 
     let day = moment().format('dddd');
@@ -7,16 +9,81 @@ $(document).ready(function(){
 
     $("#currentDay").text(day + ", " + monthAndDay);
 
-    // on click listener
-    $(".btn").on("click", function(event){
+    // on click listener 9 am
+    $("#9am").on("click", function(event){
         event.preventDefault();
         
-        // Turn text into readonly
-        console.log($(".form-control").val());
+        localStorage.setItem("eventAt9", $("#9am-block").val());
     })
 
-    // storing items into local storage
-    function storeEvent(){
+    // on click listener 10 am
+    $("#10am").on("click", function(event){
+        event.preventDefault();
+        
+        localStorage.setItem("eventAt10", $("#10am-block").val());
+    })
+
+    // on click listener 11 am
+    $("#11am").on("click", function(event){
+        event.preventDefault();
+        
+        localStorage.setItem("eventAt11", $("#11am-block").val());
+    })
+
+    // on click listener 12 pm
+    $("#12pm").on("click", function(event){
+        event.preventDefault();
+        
+        localStorage.setItem("eventAt12", $("#12pm-block").val());
+    })
+
+    // on click listener 1 pm
+    $("#1pm").on("click", function(event){
+        event.preventDefault();
+        
+        localStorage.setItem("eventAt1", $("#1pm-block").val());
+    })
+
+    // on click listener 2 pm
+    $("#2pm").on("click", function(event){
+        event.preventDefault();
+        
+        localStorage.setItem("eventAt2", $("#2pm-block").val());
+    })
+
+    // on click listener 3 pm
+    $("#3pm").on("click", function(event){
+        event.preventDefault();
+        
+        localStorage.setItem("eventAt3", $("#3pm-block").val());
+    })
     
+    // on click listener 4 pm
+    $("#4pm").on("click", function(event){
+        event.preventDefault();
+        
+        localStorage.setItem("eventAt4", $("#4pm-block").val());
+    })
+
+    // on click listener 5 pm
+    $("#5pm").on("click", function(event){
+        event.preventDefault();
+        
+        localStorage.setItem("eventAt5", $("#5pm-block").val());
+    })
+
+    function init(){
+        localStorage.getItem("eventAt9");
+        localStorage.getItem("eventAt10");
+        localStorage.getItem("eventAt11");
+        localStorage.getItem("eventAt12");
+        localStorage.getItem("eventAt1");
+        localStorage.getItem("eventAt2");
+        localStorage.getItem("eventAt3");
+        localStorage.getItem("eventAt4");
+        localStorage.getItem("eventAt5");
+        
     }
+    
+
 })
