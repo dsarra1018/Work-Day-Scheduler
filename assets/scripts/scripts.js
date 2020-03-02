b95d9ec7d701ba2ddc728f916e8e4fcdb0061606
Darren Sarra <adriansarra18@gmail.com>
2020-03-01 21:30:32 -0500

    Update textarea into an input. Added a test listener.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
 
+
     // Displaying todays date 
     let day = moment().format('dddd');
     let monthAndDay = moment().format('MMMM Do');
 
     $("#currentDay").text(day + ", " + monthAndDay);
 
+    // on click listener
+    $(".btn").on("click", function(event){
+        event.preventDefault();
+        
+        // Turn text into readonly
+        console.log($(".form-control").val());
+    })
+
+    // storing items into local storage
+    function storeEvent(){
     
-
-
-
-
-
-
+    }
 })
-
-
-
